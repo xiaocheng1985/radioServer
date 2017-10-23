e9edcefa05513c95e4fb588422f266658500bd0b
肖成 <xiaocheng@hupu.com>
2017-10-23 16:34:07 +0800

    init

---
+++ js/JavaScript.js
@@ -0,0 +1,4 @@
+// 鑾峰彇绐楀彛楂樺害
+wheight = window.innerHeight
+document.getElementsByTagName('body')[0].style.height = wheight + 'px';
+document.getElementById('logo').style.marginTop = (wheight * 0.35 - 80) + 'px';
